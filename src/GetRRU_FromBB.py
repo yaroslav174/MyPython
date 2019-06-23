e275950961f06300885587e2173cf351d13f9036
yaroslav174 <yaroslav.tselkovsky@gmail.com>
2019-06-23 20:17:46 +0300

    secondCommit

--- src/GetRRU_FromBB.py
+++ src/GetRRU_FromBB.py
@@ -2,22 +2,22 @@
 def Term():
 	open('RRUS.log','w').close()	 #clear RRUS.log
 	from subprocess import Popen
-	args="amosbatch -p 2 listBS1.txt 'lt all;rbs;RBSEricsson12#;l+ RRUS.log; get rru prod; l-;'"	
+	args="amosbatch -p 2 listBS1.txt 'lt all;rbs;RBSEricsson12#;l+ RRUS.log; get rru prod; l-;'" #amosbatch function for each value from file "list1.txt", execution some commands and write log
 	process=Popen(args, shell=True)
 	process.wait()
 
 
-def GetIn():
-	f=open('RRUS.log')
-	for line in f:
+def GetIn():  						#search necessary parameters (type of RRUS(remote radio unit))
+	f=open('RRUS.log')  			# open log
+	for line in f:     				# concurrence string with "CH"
 		if 'CH' in line:
-			print line.split('>')[0]
-		elif 'productName' in line:
+			print line.split('>')[0] #print value more null
+		elif 'productName' in line:  # for string with "productName"
 			#print line.split(' ')[4].rstrip()
-			print line.split('=')[1].rstrip()
-		elif 'serialNumber' in line:
+			print line.split('=')[1].rstrip() #print value from right side of "productName"
+		elif 'serialNumber' in line: # for string with "serialNumber"
 			#print line.split(' ')[4].rstrip()
-			print line.split('=')[1]	
+			print line.split('=')[1] #print value from right side of "serialNumber"
 
 Term()
 GetIn()
--- src/Open_CE.py
+++ src/Open_CE.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 def TermOSSCE():
-	from subprocess import Popen
-	args="/opt/ericsson/bin/cex_client"	
-	process=Popen(args, shell=True)
+	from subprocess import Popen   				 #inport Popen for subprocess
+	args="/opt/ericsson/bin/cex_client"		     #set path of application for var "args(argument)"
+	process=Popen(args, shell=True)				 #open application
 	#process.wait()
 	
-def TermRNCE69():	
+def TermRNCE69():								 #function for RNCE69
 	
-	import subprocess
+	import subprocess							
 	from subprocess import Popen
-	cmd1 = ['/bin/gnome-terminal -e "/home/ytselk/cli/Python/RNCE-CHE069"']
-	cmd2 = 'lt all'
-	a = subprocess.Popen(cmd1, shell=True)
+	cmd1 = ['/bin/gnome-terminal -e "/home/ytselk/cli/Python/RNCE-CHE069"'] #path for terminal and command to open executable file
+	#cmd2 = 'lt all'
+	a = subprocess.Popen(cmd1, shell=True) 								    #open terminal and run executable file "RNCE-CHE069" with a some comands
 	
 		
 def TermRNCE74():
@@ -19,7 +19,7 @@ def TermRNCE74():
 	import subprocess
 	from subprocess import Popen
 	cmd1 = ['/bin/gnome-terminal -e "/home/ytselk/cli/Python/RNCE-CHE074"']
-	cmd2 = 'lt all'
+	#cmd2 = 'lt all'
 	a = subprocess.Popen(cmd1, shell=True)
 
 def TermRNCE131():
@@ -27,7 +27,7 @@ def TermRNCE131():
 	import subprocess
 	from subprocess import Popen
 	cmd1 = ['/bin/gnome-terminal -e "/home/ytselk/cli/Python/RNCE-CHE131"']
-	cmd2 = 'lt all'
+	#cmd2 = 'lt all'
 	a = subprocess.Popen(cmd1, shell=True)
 
 def TermRNCE129():
@@ -35,10 +35,10 @@ def TermRNCE129():
 	import subprocess
 	from subprocess import Popen
 	cmd1 = ['/bin/gnome-terminal -e "/home/ytselk/cli/Python/RNCE-CHE129"']
-	cmd2 = 'lt all'
+	#cmd2 = 'lt all'
 	a = subprocess.Popen(cmd1, shell=True)
 
-TermOSSCE()
+TermOSSCE()  #sequence for functions
 TermRNCE69()
 TermRNCE74()
 TermRNCE129()
